fa62bb40de3a47932f5663d76dd8935990f345cb
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 17:27:32 -0200

    Should two products with different names be different

--- src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
+++ src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
@@ -1,5 +1,7 @@
 package com.thiagobaptista.tddcrudstudy.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import com.thiagobaptista.tddcrudstudy.Product;
@@ -11,5 +13,17 @@ public class MainTest
 	{
 		Product p = new Product();
 		p.setName("Notebook Negativo");
+	}
+	
+	@Test
+	public void should_two_products_with_different_names_be_different()
+	{
+		Product p1 = new Product();
+		p1.setName("Notebook Negativo");
+		
+		Product p2 = new Product();
+		p2.setName("Comically-oversized Lightsaber");
+		
+		assertNotEquals(p1, p2);
 	}
 }
