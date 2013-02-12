bd765cbc7a6b41b28352825f51fe1b06e54266f2
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 17:33:13 -0200

    [RED] Setting the same name to two Products should make them have equal names

--- src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
+++ src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
@@ -19,4 +19,16 @@ public class MainTest
 		
 		assertNotEquals(p1, p2);
 	}
+	
+	@Test
+	public void should_two_products_with_equal_names_have_the_same_name()
+	{
+		Product p1 = new Product();
+		p1.setName("Narnia Closet");
+		
+		Product p2 = new Product();
+		p2.setName("Narnia Closet");
+		
+		assertEquals(p1.getName(), p2.getName());
+	}
 }
