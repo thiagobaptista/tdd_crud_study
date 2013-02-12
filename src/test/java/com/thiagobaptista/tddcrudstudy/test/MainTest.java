1bfc659c6724e2c4ab7e2ee91ee2e1026377b6f6
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 19:32:18 -0200

    [RED] Fetching registered Products from the Catalog

--- src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
+++ src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
@@ -2,6 +2,8 @@ package com.thiagobaptista.tddcrudstudy.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import com.thiagobaptista.tddcrudstudy.Catalog;
@@ -18,5 +20,19 @@ public class MainTest
 		
 		Catalog c = new Catalog();
 		c.register(p);
+	}
+	
+	@Test
+	public void should_fetch_previously_registered_product_from_catalog()
+	{
+		Product p = new Product();
+		p.setName("Flux Capacitor");
+		p.setCategory("Time-travelling Wares");
+		
+		Catalog c = new Catalog();
+		c.register(p);
+		
+		List<Product> registeredProducts = c.getAllProducts();
+		assertTrue( registeredProducts.contains(p) );
 	}
 }
