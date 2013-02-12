7f16a6b51442e345c2ae70be6ed9f260d3cb68bb
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 19:15:34 -0200

    [REFACTOR] Extracted Product's test to a class of their own

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ tdd_crud_study
 
 [![Build Status](https://travis-ci.org/thiagobaptista/tdd_crud_study.png)](https://travis-ci.org/thiagobaptista/tdd_crud_study)
 
-A small study on Test-driven, incremental building CRUD development
+A small study on Test-driven, incremental building CRUD development.
+
+I would encourage anyone interested in doing so to accompany this study through this project's git logs and commit history.
--- src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
+++ src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
@@ -8,55 +8,7 @@ import com.thiagobaptista.tddcrudstudy.Catalog;
 import com.thiagobaptista.tddcrudstudy.Product;
 
 public class MainTest
-{
-	@Test
-	public void should_two_products_with_different_names_be_different()
-	{
-		Product p1 = new Product();
-		p1.setName("Notebook Negativo");
-		
-		Product p2 = new Product();
-		p2.setName("Comically-oversized Lightsaber");
-		
-		assertNotEquals(p1, p2);
-	}
-	
-	@Test
-	public void should_products_from_different_categories_be_different()
-	{
-		Product p1 = new Product();
-		p1.setCategory("Electronics");
-		
-		Product p2 = new Product();
-		p2.setCategory("Books");
-		
-		assertNotEquals(p1, p2);
-	}
-	
-	@Test
-	public void should_two_products_with_equal_names_have_the_same_name()
-	{
-		Product p1 = new Product();
-		p1.setName("Narnia Closet");
-		
-		Product p2 = new Product();
-		p2.setName("Narnia Closet");
-		
-		assertEquals(p1.getName(), p2.getName());
-	}
-	
-	@Test
-	public void should_two_products_with_different_names_be_named_differently()
-	{
-		Product p1 = new Product();
-		p1.setName("Narnia Closet");
-		
-		Product p2 = new Product();
-		p2.setName("Willow-with-Dragon-heart-string Wand");
-		
-		assertNotEquals(p1.getName(), p2.getName());
-	}
-	
+{	
 	@Test
 	public void should_register_a_product_into_a_catalog()
 	{
