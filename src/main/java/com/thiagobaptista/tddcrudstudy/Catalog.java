411a08369ba9b79da930208553632ac9c18b9dcc
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 19:45:12 -0200

    [GREEN] Did I do a too big step forward?

--- src/main/java/com/thiagobaptista/tddcrudstudy/Catalog.java
+++ src/main/java/com/thiagobaptista/tddcrudstudy/Catalog.java
@@ -1,16 +1,24 @@
 package com.thiagobaptista.tddcrudstudy;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Catalog
 {
+	private Collection<Product> registeredProducts;
+	
 	public void register(Product p)
 	{
-		// TODO Auto-generated method stub
+		if (registeredProducts == null)
+		{
+			registeredProducts = new ArrayList<Product>(0);
+		}
+		
+		registeredProducts.add(p);
 	}
 
 	public Collection<Product> getAllProducts()
 	{
-		return null;
+		return registeredProducts;
 	}
 }
