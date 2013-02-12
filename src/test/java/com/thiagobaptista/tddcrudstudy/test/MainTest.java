28735bdf5e7c76597427fed03feba0dc7b780281
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 19:46:00 -0200

    [REFACTOR] Removing no longer necessary test

--- src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
+++ src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
@@ -12,17 +12,6 @@ import com.thiagobaptista.tddcrudstudy.Product;
 public class MainTest
 {	
 	@Test
-	public void should_register_a_product_into_a_catalog()
-	{
-		Product p = new Product();
-		p.setName("Flux Capacitor");
-		p.setCategory("Time-travelling Wares");
-		
-		Catalog c = new Catalog();
-		c.register(p);
-	}
-	
-	@Test
 	public void should_fetch_previously_registered_product_from_catalog()
 	{
 		Product p = new Product();
