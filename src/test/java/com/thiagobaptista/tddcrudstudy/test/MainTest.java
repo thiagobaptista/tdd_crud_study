98dbbcbdd6efb817cd99fcdbcee5f32de5a8e958
Thiago Baptista <contato@thiagobaptista.com>
2013-02-12 16:39:49 -0200

    [RED] A Product should have a name

--- src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
+++ src/test/java/com/thiagobaptista/tddcrudstudy/test/MainTest.java
@@ -6,9 +6,16 @@ import com.thiagobaptista.tddcrudstudy.Product;
 
 public class MainTest
 {
+	@SuppressWarnings("unused")
 	@Test
 	public void should_there_be_a_product()
 	{
 		Product p = new Product();
+	}
+	
+	public void should_a_product_have_a_name()
+	{
+		Product p = new Product();
+		p.setName("Notebook Negativo");
 	}
 }
